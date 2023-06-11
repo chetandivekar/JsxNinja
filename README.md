6e75e05642bdec64ef9e0d35ef46d0df30ed20dc
chetandivekar <chetandivekar6@gmail.com>
2023-06-11 14:44:29 +0530

    Jsx Extension

---
+++ README.md
@@ -0,0 +1,41 @@
+# JSX Attribute Ninja
+
+The JSX Attribute Ninja is a Visual Studio Code extension that helps you effortlessly replace and modify attributes in JSX files. It automates the process of converting commonly used attributes in JSX, making your development workflow faster and more efficient.
+
+## Features
+
+- **Automatic attribute replacement**: The extension automatically detects and replaces specific attributes in JSX files, saving you time and effort.
+- **Attribute conversions**: It converts commonly used attributes to their appropriate JSX equivalents, ensuring your code adheres to JSX syntax conventions.
+- **Multiple attribute support**: The extension handles multiple instances of attribute replacements in a single file or across multiple files simultaneously.
+- **Customizable rules**: You can configure the extension's rules and specify your own attribute mappings to suit your project's specific needs.
+
+## Usage
+
+1. Install the "JSX Attribute Ninja" extension from the Visual Studio Code Marketplace.
+2. Open a JSX file in Visual Studio Code.
+3. Save the file to trigger the automatic attribute replacement process.
+4. Sit back and let the JSX Attribute Ninja do its magic! It will replace the specified attributes with their JSX equivalents throughout the file.
+
+## Configuration
+
+The extension provides some configuration options to customize its behavior. To access the configuration settings, follow these steps:
+
+1. Go to **File > Preferences > Settings** or use the keyboard shortcut (`Ctrl/Cmd + ,`).
+2. In the Settings tab, search for "JSX Attribute Ninja".
+3. Adjust the configuration options according to your preferences.
+
+## Contribution
+
+Contributions and feedback are welcome! If you encounter any issues or have suggestions for improvements, please visit the(https://github.com/chetandivekar) for the JSX Attribute Ninja extension.
+
+## License
+
+MIT License
+
+Copyright (c) Chetan Divekar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ extension.js
@@ -0,0 +1,104 @@
+const vscode = require("vscode");
+
+function activate(context) {
+  console.log(
+    'Congratulations, your extension "JSX-Attribute-Ninja" is now active!'
+  );
+
+  let disposable = vscode.commands.registerCommand(
+    "JSX-Attribute-Ninja.JsxNinja",
+    function () {
+      vscode.window.showInformationMessage(
+        "Hello World from JSX Attribute Ninja!"
+      );
+    }
+  );
+
+  vscode.languages.registerDocumentFormattingEditProvider(
+    ["javascriptreact", "jsx", "typescriptreact", "javascript"],
+    {
+      provideDocumentFormattingEdits(document) {
+        const edits = [];
+
+        const classRegex = /class=/g;
+        let classMatch;
+        while ((classMatch = classRegex.exec(document.getText()))) {
+          const startPos = document.positionAt(classMatch.index);
+          const endPos = document.positionAt(
+            classMatch.index + classMatch[0].length
+          );
+          const range = new vscode.Range(startPos, endPos);
+          const newText = "className=";
+          edits.push(new vscode.TextEdit(range, newText));
+        }
+
+        const hrefRegex = /href="#"/g;
+        let hrefMatch;
+        while ((hrefMatch = hrefRegex.exec(document.getText()))) {
+          const startPos = document.positionAt(hrefMatch.index);
+          const endPos = document.positionAt(
+            hrefMatch.index + hrefMatch[0].length
+          );
+          const range = new vscode.Range(startPos, endPos);
+          const newText = 'href="/"';
+          edits.push(new vscode.TextEdit(range, newText));
+        }
+
+        const inputRegex = /<input(\s+[^>]+)?(?<!\/)>\s*(?!\/)/g;
+        let inputMatch;
+        while ((inputMatch = inputRegex.exec(document.getText()))) {
+          const startPos = document.positionAt(inputMatch.index);
+          const endPos = document.positionAt(
+            inputMatch.index + inputMatch[0].length
+          );
+          const range = new vscode.Range(startPos, endPos);
+          const newText = `<input${inputMatch[1] || ""} />`;
+          edits.push(new vscode.TextEdit(range, newText));
+        }
+
+        const hrRegex = /<hr\s*(?<!\/)>\s*(?!\/)/g;
+        let hrMatch;
+        while ((hrMatch = hrRegex.exec(document.getText()))) {
+          const startPos = document.positionAt(hrMatch.index);
+          const endPos = document.positionAt(hrMatch.index + hrMatch[0].length);
+          const range = new vscode.Range(startPos, endPos);
+          const newText = "<hr />";
+          edits.push(new vscode.TextEdit(range, newText));
+        }
+
+        const brRegex = /<br\s*(?<!\/)>\s*(?!\/)/g;
+        let brMatch;
+        while ((brMatch = brRegex.exec(document.getText()))) {
+          const startPos = document.positionAt(brMatch.index);
+          const endPos = document.positionAt(brMatch.index + brMatch[0].length);
+          const range = new vscode.Range(startPos, endPos);
+          const newText = "<br />";
+          edits.push(new vscode.TextEdit(range, newText));
+        }
+
+        const forRegex = /for=/g;
+        let forMatch;
+        while ((forMatch = forRegex.exec(document.getText()))) {
+          const startPos = document.positionAt(forMatch.index);
+          const endPos = document.positionAt(
+            forMatch.index + forMatch[0].length
+          );
+          const range = new vscode.Range(startPos, endPos);
+          const newText = "htmlFor=";
+          edits.push(new vscode.TextEdit(range, newText));
+        }
+
+        return edits;
+      },
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
